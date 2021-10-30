1022c8d10aeecd60246a7692d3884db0be1e7236
Lemonade-up <wmaglangit1118@gmail.com>
2021-10-30 19:35:58 +0800

    Input Functions added

--- Program no.2_ver2.py
+++ Program no.2_ver2.py
@@ -3,8 +3,18 @@
 # Display the output in the following format.
 # The total amount is ______.
 
+def applesAmount():
+    _applesAmount = input("How many apples do you want to buy?: ")
+    return _applesAmount
+
+def orangesAmount():
+    _orangesAmount = input(" How many oranges do you want to buy?: ")
+    return _orangesAmount
+    
 # Steps
 # Ask how many apples you want to buy?
+applesAmount()
 # Ask how many oranges you want to buy?
+orangesAmount()
 # Insert Formula for Computation
 # Display
