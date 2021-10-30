b0fbf533efceef2e83fb5fa664971871b1ba981c
Lemonade-up <wmaglangit1118@gmail.com>
2021-10-30 21:21:20 +0800

    Pseudo Code added

---
+++ Program no.3_ver2.py
@@ -0,0 +1,12 @@
+# Create a program which you will enter the amount of money you have, it will also ask for the price of an apple.
+# Display the maximum number of apples that you can buy and the remaining money that you will have.
+# Display the output in the following format.
+# You can buy ___ apples and your change is ___ pesos.
+
+
+
+# Steps
+#1 Input amount of money
+#2 Input price of apple
+#3 Formula
+#4 Display
