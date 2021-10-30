178a6461a91005b1ac134c1f9b39dd37877d52ed
Lemonade-up <wmaglangit1118@gmail.com>
2021-10-30 19:28:42 +0800

    Pseudo Code

---
+++ Program no.2_ver2.py
@@ -0,0 +1,10 @@
+# Create a program that will ask how many apples and oranges you want to buy.
+# Display the total amount you need to pay if apple price is 20 pesos and orange is 25.
+# Display the output in the following format.
+# The total amount is ______.
+
+# Steps
+# Ask how many apples you want to buy?
+# Ask how many oranges you want to buy?
+# Insert Formula for Computation
+# Display
