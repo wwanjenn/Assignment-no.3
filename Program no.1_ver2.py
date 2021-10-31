b7b9e24f1094f03e280681ab2e6e0ce51b53e675
Lemonade-up <wmaglangit1118@gmail.com>
2021-10-31 22:37:59 +0800

    Input Functions added

--- Program no.1_ver2.py
+++ Program no.1_ver2.py
@@ -2,8 +2,18 @@
 # Display those details in the following format.
 # Hi, my name is _____. I am ____ years old and I live in _____ .
 
+def insert(sample):
+    _store = input(f'{sample}: ')
+    return _store
+
 # Steps
 #1 Input name
+name = insert('Name')
 #2 Input age
+age = insert('Age')
 #3 Input address
+address = insert('Address')
 #4 Display
+
+
+
