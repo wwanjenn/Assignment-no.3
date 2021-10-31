593a246f01a5d7075e368d5e88abe4a318082473
Lemonade-up <wmaglangit1118@gmail.com>
2021-10-31 22:37:06 +0800

    Pseudo Code

---
+++ Program no.1_ver2.py
@@ -0,0 +1,9 @@
+# Create a program that will ask for name, age and address. 
+# Display those details in the following format.
+# Hi, my name is _____. I am ____ years old and I live in _____ .
+
+# Steps
+#1 Input name
+#2 Input age
+#3 Input address
+#4 Display
